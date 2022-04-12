db3e4d7418dabbe5ce4a93f0e91334510c24fed4
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2022-04-12 16:28:28 +0200

    basic connecting code

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import io from "socket.io-client";
+import { EventEmitter } from "node:events";
+import MsgroomSocket from "./types/socket.io";
+
+/*export default class Client extends EventEmitter {
+    socket: MsgroomSocket;
+    
+    constructor(public name: string, )
+}*/
+
+const socket: MsgroomSocket = io("https://devel.windows96.net:4096");
+socket.emit("online");
+
+socket.on("auth-complete", userID => {
+    console.log("socket:", socket);
+
+    socket.emit("message", {
+        type   : "text",
+        content: "**I figured it out 😁**"
+    });
+});
+
+socket.emit("auth", {
+    user: "testBIGEmoji"
+});
