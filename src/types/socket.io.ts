20f4a94ed8d3521a96841b59ee200cc6fbff7fbf
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2022-04-17 17:06:58 +0200

    socket.io typings

---
+++ src/types/socket.io.ts
@@ -0,0 +1,93 @@
+import { Socket } from "socket.io-client";
+
+
+type MsgroomSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
+export default MsgroomSocket;
+
+/**
+ * Hexadecimal color string
+ * @example "#ff0000"
+ */
+export type hexColor = string;
+
+export type flag = "staff";
+
+interface User {
+    color: hexColor;
+    flags: flag[];
+    id: string;
+    session_id: string;
+    user: string;
+}
+
+
+// used for receiving events from the server
+export interface ServerToClientEvents {
+    "auth-complete": (userID: string) => void;
+    
+    message: (message: {
+        color: hexColor;
+        content: string;
+        date: string;
+        id: string;
+        session_id: string;
+        type: "text";
+        user: string
+    }) => void;
+    
+    "sys-message": (message: {
+        message: string;
+        type: "info" | "error" | "success"
+        isHtml: boolean;
+    }) => void;
+    
+    "nick-changed": (args: {
+        oldUser: string;
+        newUser: string;
+        id: string;
+        session_id: string;
+    }) => void;
+    
+    "user-join": (args: User) => void;
+    
+    "user-leave": (args: {
+        id: string;
+        session_id: string;
+        user: string;
+    }) => void;
+    
+    "user-update": (args: {
+        type: "tag-add";
+        tag?: "staff" | string;
+        tagLabel?: string;
+        user: string
+    }) => void;
+    
+    online: (members: User[]) => void;
+    
+    "auth-error": (error: {
+        reason: string;
+    }) => void;
+    
+    "werror": (reason: string) => void;
+}
+
+// used to send events to the server
+export interface ClientToServerEvents {
+    "admin-action": (args: {
+        args: any;
+    }) => void;
+    
+    message: (args: {
+        type: "text";
+        content: string;
+    }) => void;
+    
+    "change-user": (newNick: string) => void;
+    
+    online: () => void;
+    
+    auth: (args: {
+        user: string
+    }) => void;
+}
