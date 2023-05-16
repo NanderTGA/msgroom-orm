13f9802b93d4996f8eb4f1adab445d65af7835d7
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-05-16 17:54:06 +0200

    error testing

---
+++ test/errors.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from "@jest/globals";
+import { AuthError, ConnectionError, NotConnectedError } from "../src/errors";
+import Client from "../src/index";
+
+describe("Error constructors", () => {
+    it("AuthError", () => {
+        const authError = new AuthError();
+        expect(authError.name).toBe("AuthError");
+        expect(authError.message).toBe("");
+
+        const authError2 = new AuthError("test 1 2 3", { cause: "something" });
+        expect(authError2.message).toBe("test 1 2 3");
+        // @ts-ignore for testing only
+        expect(authError2.cause).toBe("something");
+    });
+
+    it("ConnectionError", () => {
+        const connectionError = new ConnectionError();
+        expect(connectionError.name).toBe("ConnectionError");
+        expect(connectionError.message).toBe("Socket.io connection error. Do the server and client version match? Did you enter the right server details? Is the server running?");
+
+        const connectionError2 = new ConnectionError("test 1 2 3", { cause: "something" });
+        expect(connectionError2.message).toBe("test 1 2 3");
+        // @ts-ignore for testing only
+        expect(connectionError2.cause).toBe("something");
+    });
+
+    it("NotConnectedError", () => {
+        const notConnectedError = new NotConnectedError();
+        expect(notConnectedError.name).toBe("NotConnectedError");
+        expect(notConnectedError.message).toBe("Not connected to a server! Connect to one first before using any other functions.");
+
+        const notConnectedError2 = new NotConnectedError("test 1 2 3", { cause: "something" });
+        expect(notConnectedError2.message).toBe("test 1 2 3");
+        // @ts-ignore for testing only
+        expect(notConnectedError2.cause).toBe("something");
+    });
+});
+
+describe("Error throwing", () => {
+    it("should throw a NotConnectedError", () => {
+        const client = new Client("TestBot");
+
+        expect( () => client.name = "fuck" ).toThrow(NotConnectedError);
+        expect( () => client.userID ).toThrow(NotConnectedError);
+        expect( () => client.sendMessage("a") ).toThrow(NotConnectedError);
+        expect( () => client.adminAction("a") ).toThrow(NotConnectedError);
+    });
+});
