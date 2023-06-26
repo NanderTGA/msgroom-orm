4b666bc50733296d464cb7c9f1f54f9c58679833
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-06-26 21:12:38 +0200

    change to normal commonjs export

--- src/utils/Command.ts
+++ src/utils/Command.ts
@@ -1,9 +1,13 @@
 import { CommandHandler } from "../types/types";
 
-export default class Command {
+class Command {
+    static default = Command;
+
     constructor(
         public description: string,
         public aliases: string[],
         public handler: CommandHandler,
     ) {}
 }
+
+export = Command;
