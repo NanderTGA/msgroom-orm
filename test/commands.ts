79ffe5bad53d8d2da30d5fca8f44a6681cce999d
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-07-03 19:27:07 +0200

    more and better tests

--- test/commands.ts
+++ test/commands.ts
@@ -6,6 +6,11 @@ import Command from "../src/utils/Command";
 
 const client = new Client("test", [ "!", "g!" ], { server: "wss://dabestmsgroomserver.com" });
 
+client.commands.something = new Command("does stuff", [ "stuff" ], () => "ok I did some stuff");
+client.commands.throwError = new Command("does exactly what you think it does", [ "inYourFace" ], () => {
+    throw new Error("fuck");
+});
+
 it("should correctly set properties", () => {
     expect(client.name).toBe("test");
     expect(client.server).toBe("wss://dabestmsgroomserver.com");
@@ -21,4 +26,35 @@ it("should validate nicknames correctly", () => {
     client.validateNickname("dfssqdfsdfqdfqfdqd"); // 18 characters
 });
 
-test.todo("built-in help command");
+test("getCommandOutput should return output of command", async () => {
+    const somethingOutput = await getCommandOutput(client, "!something");
+    expect(somethingOutput).toBe("ok I did some stuff");
+});
+
+it("should handle errors in command handlers correctly", async () => {
+    const throwErrorOutput = await getCommandOutput(client, "!throwError");
+
+    expect(throwErrorOutput).toContain("throwError");
+    expect(throwErrorOutput).toContain("Error");
+    expect(throwErrorOutput).toContain("fuck");
+});
+
+test("built-in help command should list all commands and their descriptions", async () => {
+    const helpOutput = await getCommandOutput(client, "!help");
+    
+    expect(helpOutput).toContain("help");
+
+    expect(helpOutput).toContain("something");
+    expect(helpOutput).toContain("does stuff");
+    
+    expect(helpOutput).toContain("throwError");
+    expect(helpOutput).toContain("does exactly what you think it does");
+});
+
+test("built-in help command should list specific commands and their descriptions", async () => {
+    const helpOutput = await getCommandOutput(client, "!help something");
+    
+    expect(helpOutput).toContain("something");
+    expect(helpOutput).toContain("does stuff");
+    expect(helpOutput).toContain("something");
+});
