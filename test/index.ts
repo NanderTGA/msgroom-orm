b8c4a695bc5481d3af27b3c0b02fd4ce7ea4b7d5
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-07-05 19:14:16 +0200

    rename testbot in test

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import { it } from "@jest/globals";
 import Client from "../src/index";
 
 it("should connect, send a message and disconnect", async () => {
-    const client = new Client("[!] TestBot", "!");
+    const client = new Client("JestBot", "very long prefix so nobody can trigger it!");
     const messageToSend = "Hi there! Sorry for the spam.";
     await client.connect();
 
