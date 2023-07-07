628691025a324f8b09c7060eb1b660979d510d21
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-07-07 19:36:57 +0200

    fix error on node

--- src/utils/walkDirectory.ts
+++ src/utils/walkDirectory.ts
@@ -4,11 +4,11 @@ import type { walk } from "@nodelib/fs.walk";
 let walkDirectory: typeof walk.__promisify__;
 try {
     // eslint-disable-next-line @typescript-eslint/no-implied-eval
-    Function(`
+    walkDirectory = Function("require", "promisify", `
         // try breaking this, rollup
         const { walk } = require("@nodelib/fs.walk");
-        walkDirectory = promisify(walk);
-    `)();
+        return promisify(walk);
+    `)(require, promisify) as typeof walk.__promisify__;
 } catch {
     // eslint-disable-next-line @typescript-eslint/require-await
     walkDirectory = async () => {
