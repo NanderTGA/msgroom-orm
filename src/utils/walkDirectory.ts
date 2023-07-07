e077f99a6e5353e637e816bd6aa06a4cc69e9ada
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-07-07 17:55:03 +0200

    attempt at detecting browsers

--- src/utils/walkDirectory.ts
+++ src/utils/walkDirectory.ts
@@ -1,13 +1,16 @@
 import { promisify } from "util";
 import type { walk } from "@nodelib/fs.walk";
 
+const isNode = typeof process != "undefined" && typeof process.versions?.node == "string";
+
 let walkDirectory: typeof walk.__promisify__;
-try {
+if (isNode) {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { walk } = require("@nodelib/fs.walk") as typeof import("@nodelib/fs.walk");
     walkDirectory = promisify(walk);
-} catch {
-    walkDirectory = () => {
+} else {
+    // eslint-disable-next-line @typescript-eslint/require-await
+    walkDirectory = async () => {
         throw new Error("This feature is not available in this environment!");
     };
 }
