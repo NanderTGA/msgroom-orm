722395d7fe366c55ac3349242c6a770a588bbd75
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-07-07 23:15:47 +0200

    move when to connect

--- test/index.ts
+++ test/index.ts
@@ -4,11 +4,11 @@ import Client from "../src/index";
 it("should connect, send a message and disconnect", async () => {
     const client = new Client("JestBot", "very long prefix so nobody can trigger it!");
     const messageToSend = "Hi there! Sorry for the spam.";
-    await client.connect();
-
+    
     client.on("message", message => {
         if (message.author.ID == client.ID && message.content == messageToSend) client.disconnect();
     });
-
+    
+    await client.connect();
     client.sendMessage(messageToSend);
 });
