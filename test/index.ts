5258233bebda97aff62607b485f58b4dfb8e8380
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-07-09 19:12:40 +0200

    fix JestBot not always leaving

--- test/index.ts
+++ test/index.ts
@@ -2,13 +2,18 @@ import { it } from "@jest/globals";
 import Client from "../src/index";
 
 it("should connect, send a message and disconnect", async () => {
-    const client = new Client("JestBot", "very long prefix so nobody can trigger it!");
+    const client = new Client("JestBot", "very long prefix so nobody can trigger it!", { blockSelf: false });
     const messageToSend = "Hi there! Sorry for the spam.";
-    
-    client.on("message", message => {
-        if (message.author.ID == client.ID && message.content == messageToSend) client.disconnect();
+
+    const done = new Promise<void>( resolve => {
+        client.on("message", message => {
+            if (!(message.author.ID == client.ID && message.content == messageToSend)) return;
+            client.disconnect();
+            resolve();
+        });
     });
-    
+        
     await client.connect();
     client.sendMessage(messageToSend);
+    await done;
 });
