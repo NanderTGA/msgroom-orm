09ba1bc75beaf59c88df8c2902693c92f5674b6d
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-09-19 22:49:22 +0200

    docs: correct repo name

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
     url    : "https://nandertga.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/msgroom",
+    baseUrl: "/msgroom-orm",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
