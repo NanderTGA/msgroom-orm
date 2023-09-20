be4386b22d5e156541b3d7b737b7dff18c85b3bf
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-09-20 22:32:27 +0200

    docs: another attempt at getting this to build

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
     organizationName: "NanderTGA", // Usually your GitHub org/user name.
     projectName     : "msgroom", // Usually your repo name.
 
-    onBrokenLinks        : "throw",
+    onBrokenLinks        : "warn",
     onBrokenMarkdownLinks: "warn",
 
     // Even if you don't use internalization, you can use this field to set useful
