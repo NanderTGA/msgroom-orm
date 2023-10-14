9589fcd74f74682f0d6752d086fad9d33e4e81d8
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-10-14 21:53:35 +0200

    refactor: remove IIFE we don't need anymore
    I had to use an immediately invoked function expression (IIFE) because
    back then we were using commonJS which doesn't have top-level await.
    Now we're using ESM, which does have it.

--- src/example/example.ts
+++ src/example/example.ts
@@ -2,77 +2,76 @@ import Client from "#client";
 
 import { formatWithOptions } from "node:util";
 
-void (async () => {
-    const client = new Client("[!] TestBot", [ "!", /^test!/i ], {
-        printErrors: true,
-        helpSuffix : `
+const client = new Client("[!] TestBot", [ "!", /^test!/i ], {
+    printErrors: true,
+    helpSuffix : `
 *TestBot is a bot made by NanderTGA to test new [MsgRoom.js](https://nandertga.github.io/msgroom-orm/) features before releasing them.
 Some things may also seem broken when they are intentionally broken so I can test a specific feature.
 Since it is completely useless, it will go offline once I'm done testing things.*
 `.trim(),
-        blockSelf: true,
-    });
+    blockSelf: true,
+});
 
-    client.on("message", message => {
-        const monkey: Record<string, string> = {
-            "hey"        : "hey",
-            "gn"         : "gn",
-            "gm"         : "gm",
-            "goodmorning": "goodmorning",
-            "goodnight"  : "goodnight",
-            "who's joe?" : "Joe mama",
-            "who's joe"  : "Joe mama",
-        };
-        if (monkey[message.content]) return void client.sendMessage(monkey[message.content]);
-    });
+client.on("message", message => {
+    const monkey: Record<string, string> = {
+        "hey"        : "hey",
+        "gn"         : "gn",
+        "gm"         : "gm",
+        "goodmorning": "goodmorning",
+        "goodnight"  : "goodnight",
+        "who's joe?" : "Joe mama",
+        "who's joe"  : "Joe mama",
+    };
+    if (monkey[message.content]) return void client.sendMessage(monkey[message.content]);
+});
 
-    client.on("werror", reason => void console.warn("\nReceived werror:", reason));
+client.on("werror", reason => void console.warn("\nReceived werror:", reason));
 
-    client.commands.ping = {
-        description: "Ping pong!",
-        handler    : () => "Pong!",
-        subcommands: {
-            pong: { // this is any normal command, which can have its own subcommands and aliases and such
-                description: "Ping pong peng!",
-                handler    : () => "Peng!",
-                aliases    : [
-                    "peng",
-                    [ "ping", "peng" ],
-                ],
-            },
+client.commands.ping = {
+    description: "Ping pong!",
+    handler    : () => "Pong!",
+    subcommands: {
+        pong: { // this is any normal command, which can have its own subcommands and aliases and such
+            description: "Ping pong peng!",
+            handler    : () => "Peng!",
+            aliases    : [
+                "peng",
+                [ "ping", "peng" ],
+            ],
         },
-        aliases: [
-            "pang",
-        ],
-    };
+    },
+    aliases: [
+        "pang",
+    ],
+};
 
-    client.commands.repeat = {
-        description: "Repeats what you said.",
-        handler    : (context, ...args) => args.join(" "),
-    };
+client.commands.repeat = {
+    description: "Repeats what you said.",
+    handler    : (context, ...args) => args.join(" "),
+};
 
-    client.commands.sendTest = {
-        description: "Uses `context.send()` and `context.reply()` to send some replies.",
-        handler    : context => {
-            context.send("Sent!");
-            context.reply("Replied!");
-        },
-    };
+client.commands.sendTest = {
+    description: "Uses `context.send()` and `context.reply()` to send some replies.",
+    handler    : context => {
+        context.send("Sent!");
+        context.reply("Replied!");
+    },
+};
 
-    client.commands.showContext = {
-        description: "Returns the context argument.",
-        handler    : context => formatWithOptions({ compact: false, colors: false }, context),
-    };
+client.commands.showContext = {
+    description: "Returns the context argument.",
+    handler    : context => formatWithOptions({ compact: false, colors: false }, context),
+};
 
-    client.commands.testError = {
-        description: "Throws an error.",
-        aliases    : [
-            [ "errorTest" ],
-        ],
-        handler: () => {
-            throw new Error("fuck");
-        },
-    };
+client.commands.testError = {
+    description: "Throws an error.",
+    aliases    : [
+        [ "errorTest" ],
+    ],
+    handler: () => {
+        throw new Error("fuck");
+    },
+};
 
 client.commands.name = {
     description: "Tells you my name or changes it.",
@@ -83,9 +82,8 @@ client.commands.name = {
     },
 };
 
-    await client.loadDirectory(new URL("./modules", import.meta.url));
-    await client.connect();
+await client.loadDirectory(new URL("./modules", import.meta.url));
+await client.connect();
 
-    // will cause a werror
-    client.sendMessage("a".repeat(2049));
-})();
+// will cause a werror
+client.sendMessage("a".repeat(2049));
