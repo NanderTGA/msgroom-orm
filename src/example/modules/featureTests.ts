e6090cbc0606c034003947743bb76c96beb5a28b
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-12-19 21:24:29 +0100

    test(testbot): remove changing of bot name from name command

--- src/example/modules/featureTests.ts
+++ src/example/modules/featureTests.ts
@@ -3,11 +3,7 @@ import type { ModuleInitializeFunction, CommandMap } from "#types";
 const initialize: ModuleInitializeFunction = client => ({
     name: {
         description: "Tells you my name or changes it.",
-        handler    : (context, ...nameParts) => {
-            const newName = nameParts.join(" ");
-            if (!newName) return client.name;
-            client.name = newName;
-        },
+        handler    : () => client.name,
     },
     showArguments: {
         description: "Shows the arguments passed to this command.",
