3a153429515b239ed7f1dd8d2c3e2ef9fe114a8f
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2024-01-07 16:13:39 +0100

    chore: replace array-starts-with with our own code
    the dependency was a pain to import and now we can possibly get a performance improvement in the future

--- src/utils/array-starts-with.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "array-starts-with" {
-    export type ArrayStartsWith = (base: unknown[], start: unknown[]) => boolean;
-
-    const arrayStartsWith: {
-        default: ArrayStartsWith;
-    } | ArrayStartsWith;
-    export default arrayStartsWith;
-}
--- src/utils/arrayStartsWith.ts
+++ src/utils/arrayStartsWith.ts
@@ -1,7 +1,14 @@
-import arrayStartsWithExports, { ArrayStartsWith } from "array-starts-with";
-
-let arrayStartsWith: ArrayStartsWith;
-if (typeof arrayStartsWithExports == "function") arrayStartsWith = arrayStartsWithExports;
-else arrayStartsWith = arrayStartsWithExports.default;
-
-export default arrayStartsWith;
+export default function arrayStartsWith(array: unknown[], subArray: unknown[]): boolean {
+    /**
+     * TODO Will this improve performance?
+     * TODO I should investigate the flow of the command resolution algorithm a bit more to figure this out
+     * Big O notations:
+     * with check: O(min(array.length, subArray.length))
+     * without it: O(subArray.length)
+     */
+    // if (subArray.length > array.length) return false;
+    for (let i = 0; i < subArray.length; i++) {
+        if (subArray[i] !== array[i]) return false;
+    }
+    return true;
+}
