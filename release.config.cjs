16da080c8563c7eff486d5ce365c3f67aa78766d
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2024-01-28 16:30:19 +0100

    ci: setup semantic-release

---
+++ release.config.cjs
@@ -0,0 +1,37 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+    branches: [
+        {
+            name   : "main",
+            channel: false,
+        },
+    ],
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset      : "conventionalcommits",
+                presetConfig: {
+                    types: [
+                        { type: "fix", section: "Bug Fixes" },
+                        { type: "feat", section: "Features" },
+                        { type: "chore", hidden: true },
+                        { type: "docs", hidden: true },
+                        { type: "style", hidden: true },
+                        { type: "refactor", hidden: true },
+                        { type: "perf", section: "Performance Improvements" },
+                        { type: "test", hidden: true },
+                        { type: "build", hidden: true },
+                        { type: "ci", hidden: true },
+                        { type: "revert", hidden: true },
+                    ],
+                },
+            },
+        ],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/github",
+    ],
+};
